aa244e88f719d7921796644ef4c61a340cd8c48d
konrad.salkowski <konrad.salkowski@britishcouncil.org>
2023-04-05 22:25:12 +0200

    feat: add more properties to User aggregate

--- src/api/Features/Users/Domain/Users/User.cs
+++ src/api/Features/Users/Domain/Users/User.cs
@@ -1,6 +1,27 @@
-namespace Lothus.Users.Domain.Users;
+using System;
 
+namespace Lothus.Features.Users.Domain.Users;
+
+// TODO: to inherit from base entity and aggregate classes/interfaces
 public class User
 {
+    protected User(string username)
+    {
+        Id = Guid.NewGuid();
+        UserName = username;
+    }
+
     public Guid Id { get; private set; }
+
+    public string UserName { get; private set; }
+
+    public byte[] PasswordHash { get; private set; }
+
+    public byte[] PasswordSalt { get; private set; }
+
+    
+    public static User Create(string username)
+    {
+        return new User(username);
+    }
 }
